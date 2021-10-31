4ca584292e8ad073541a35eae7c4087fb1678011
CodewithMaaz <93466155+CodewithMaaz@users.noreply.github.com>
2021-10-31 00:42:20 -0700

    Create main.py
    copy this code if u want

---
+++ main.py
@@ -0,0 +1,14 @@
+# find area  of circle
+
+print("Find Area of circle ")
+
+radius = input("Enter Radius ")
+radius = int(radius)
+pie = 3.142
+
+area = pie * (radius * radius)
+
+print("Area Of Circle is    " + str(area))
+
+
+# code by code with maaz
